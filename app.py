3a51fda9ba19c73a138ce45ccfa2dc9a66d05062
João Walter Amadeu <joao.amadeu@unifio.edu.br>
2022-06-03 00:17:02 -0300

    CORS

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ import shortuuid
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_cors import CORS, cross_origin
+
 dbcontroll = pylocaldatabase.databasecontroller(path="database.json")
 scheduler = BackgroundScheduler()
 app = Flask(__name__)
+CORS(app)
 limiter = Limiter(
     app,
     key_func=get_remote_address,
